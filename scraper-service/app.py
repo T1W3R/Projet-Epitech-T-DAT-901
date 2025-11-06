27dc4188c92d143433b4208cbb2745b01c72d484
Alexis-Bizet <alexisbzt59@gmail.com>
2025-11-06 10:11:05 +0100

    create scraper

--- scraper-service/app.py
+++ scraper-service/app.py
@@ -1,15 +1,48 @@
+import requests, json, time
 from kafka import KafkaProducer
-import json
-import time
+
+TOPIC = "crypto_prices"
 
 producer = KafkaProducer(
-    bootstrap_servers="kafka:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    bootstrap_servers="redpanda:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    acks='all',       # attendre la confirmation
+    retries=5,
+    linger_ms=10
 )
 
-TOPIC = "crypto_news"
+print("Scraper connected to Redpanda ✅")
 
 while True:
-    msg = {"crypto": "BTC", "price": 68000, "time": time.time()}
-    producer.send(TOPIC, msg)
-    time.sleep(5)
+    try:
+        response = requests.get(
+            "https://api.coingecko.com/api/v3/coins/markets",
+            params={
+                "vs_currency": "usd",
+                "order": "market_cap_desc",
+                "per_page": 100,
+                "page": 1,
+                "sparkline": "false"
+            }
+        ).json()
+
+        for coin in response:
+            msg = {
+                "crypto": coin["symbol"].upper(),
+                "price": coin["current_price"],
+                "market_cap": coin["market_cap"],
+                "volume": coin["total_volume"],
+                "time": time.time()
+            }
+            print("Message:", msg, flush=True)
+            try:
+                producer.send(TOPIC, msg)
+            except Exception as e:
+                print("Erreur Kafka pour", coin["symbol"], e)
+
+        producer.flush()
+        print(f"Sent {len(response)} coins to Kafka")
+        time.sleep(60)  # update toutes les minutes
+    except Exception as e:
+        print("Error fetching data:", e)
+        time.sleep(10)
